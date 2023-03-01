c9dc21bb995ba78cefa1a2f03f2a8e701e1f7c6e
Lucas Zurbuchen <85756834+ljz112@users.noreply.github.com>
2023-03-01 13:56:39 -0600

    Update TableStats.java

--- src/java/simpledb/optimizer/TableStats.java
+++ src/java/simpledb/optimizer/TableStats.java
@@ -71,6 +71,10 @@ public class TableStats {
     //other fields
     private int tcard;
     private TupleDesc td;
+    private ArrayList<Integer> intInds;
+    private ArrayList<Integer> strInds;
+    private ArrayList<IntHistogram> intHists;
+    private ArrayList<StringHistogram> strHists;
 
     /**
      * Create a new TableStats object, that keeps track of statistics on each
@@ -93,8 +97,85 @@ public class TableStats {
         this.ioCostPerPage = ioCostPerPage;
         DbFile dbf = Database.getCatalog().getDatabaseFile(tableid);
         this.td = dbf.getTupleDesc();
-        //also td.getSize()
-        this.tcard = td.getSize();
+        //tcard should be the number of pages in the table 
+        this.tcard = dbf.numPages();
+        //need to scan table
+        HeapFileIterator tableIt = dbf.iterator();
+        //scan 1: find the max and min of each field for int histograms
+        tableIt.open();
+        //log indices that have array vs string inds
+        this.intInds = new ArrayList<Integer>();
+        this.strInds = new ArrayList<Integer>();
+        for (int i = 0; i < this.td.numFields(); i++) {
+            if (this.td.getFieldType(i) == Type.INT_TYPE) {
+                intInds.add(i);
+            } else {
+                strInds.add(i);
+            }
+        }
+        //keep track of [min, max] for all int fields
+        ArrayList<IntField[]> hiLo = new ArrayList<IntField[]>();
+        boolean starting = true;
+        while (tableIt.hasNext()) {
+            Tuple temp = tableIt.next();
+            //looping over intInds
+            for (int i = 0; i < intInds.size(); i++) {
+                int tupInd = intInds.get(i);
+                IntField fval = temp.getField(tupInd);
+                //case for start: intiialize everything and set
+                if (starting) {
+                    IntField[] startHiLo = {fval, fval};
+                    hiLo.add(startHiLo);
+                } else {
+                    //no more than 1 if statement can pass each iteration
+                    IntField lower = hiLo.get(i)[0];
+                    IntField upper = hiLo.get(i)[1];
+                    //check for min
+                    if (fval.compare(Predicate.Op.LESS_THAN, lower)) {
+                        hiLo.set(i, {fval, upper});
+                    }
+                    //check for max
+                    if (fval.compare(Predicate.Op.GREATER_THAN, upper)) {
+                        hiLo.set(i, {lower, fval});
+                    }
+                }
+            }
+            starting = false;
+        }
+        //make histograms with these values 
+        this.intHists = new ArrayList<IntHistogram>();
+        this.strHists = new ArrayList<StringHistogram>();
+        int intI = 0;
+        for (int i = 0; i < this.td.numFields(); i++) {
+            if (this.td.getFieldType(i) == Type.INT_TYPE) {
+                IntField[] tempEntry = hiLo.get(intI);
+                intHists.add(new IntHistogram(NUM_HIST_BINS, tempEntry[0].getValue(), tempEntry[1].getValue()));
+                intI++;
+            } else {
+                strHists.add(new StringHistogram(NUM_HIST_BINS));
+            }
+        }
+        //scan 2, read all values into their histograms
+        tableIt.rewind();
+        while (tableIt.hasNext()) {
+            Tuple temp = tableIt.next();
+            intI = 0;
+            int strI = 0;
+            for (int i = 0; i < this.td.numFields(); i++) {
+                if (this.td.getFieldType(i) == Type.INT_TYPE) {
+                    IntHistogram tempHist = intHists.get(intI);
+                    tempHist.addValue(temp.getField(i).getValue());
+                    intHists.set(intI, tempHist);
+                    intI++;
+                } else {
+                    StringHistogram tempHist = strHists.get(strI);
+                    tempHist.addValue(temp.getField(i).getValue());
+                    strHists.set(strI, tempHist);
+                    strI++;
+                }
+            }
+        }
+        tableIt.close();
     }
 
     /**
@@ -138,7 +219,21 @@ public class TableStats {
      */
     public double avgSelectivity(int field, Predicate.Op op) {
         // TODO: some code goes here
-        return 1.0;
+        //not sure if we need this or if it's right
+        //look in intHist
+        for (int i = 0; i < intInds.size(); i++) {
+            if (intInds.get(i) == field) {
+                return intHists.get(i).avgSelectivity();
+            }
+        }
+        //look in strHist
+        for (int i = 0; i < strInds.size(); i++) {
+            if (strInds.get(i) == field) {
+                return strHists.get(i).avgSelectivity();
+            }
+        }
+        //should never get to here
+        return -1.0;
     }
 
     /**
@@ -153,7 +248,20 @@ public class TableStats {
      */
     public double estimateSelectivity(int field, Predicate.Op op, Field constant) {
         // TODO: some code goes here
-        return 1.0;
+        //look in intHist
+        for (int i = 0; i < intInds.size(); i++) {
+            if (intInds.get(i) == field && constant.getType() == Type.INT_TYPE) {
+                return intHists.get(i).estimateSelectivity(op, constant.getValue());
+            }
+        }
+        //look in strHist
+        for (int i = 0; i < strInds.size(); i++) {
+            if (strInds.get(i) == field && constant.getType() == Type.STRING_TYPE) {
+                return strHists.get(i).estimateSelectivity(op, constant.getValue());
+            }
+        }
+        //should never get to here
+        return -1.0;
     }
 
     /**
